ec8af6e969648819db068bcfc52d2111bbc9bc6e
hzhang47 <hzhang47@github.com>
2018-02-12 18:56:04 -0700

    W6D1ODSRepeater

---
+++ ChinookSystem/BLL/PlaylistController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using Chinook.Data.Entities;
+using ChinookSystem.DAL;
+using System.ComponentModel;
+using Chinook.Data.DTOs;
+using Chinook.Data.POCOs;
+#endregion
+
+namespace ChinookSystem.BLL
+{
+
+    [DataObject]
+    public class PlaylistController
+    {
+        [DataObjectMethod(DataObjectMethodType.Select,false)]
+        public List<ClientPlaylist> Playlist_ClientPlaylist (int trackcountlimit)
+        {
+            using (var context = new ChinookContext())
+            {              
+
+                //use of a "parameter value" on your query
+                var results = from x in context.Playlists
+                              where x.PlaylistTracks.Count > trackcountlimit
+                              select new ClientPlaylist
+                              {
+                                  playlist = x.Name,
+                                  songs = (from y in x.PlaylistTracks
+                                           select new TracksAndGenre
+                                           {
+                                               songtitle = y.Track.Name,
+                                               songgenre = y.Track.Genre.Name,
+                                           }).ToList()
+                              };
+                return results.ToList();
+            }
+        }
+        
+    }
+}
